1458eccf04521fa0f042aaf4e05dc7c20f7f5cf6
Michele Pardo <miguelalfonsopardo@gmail.com>
2021-12-20 14:41:05 +0100

    app finished
    back-end of band app

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,44 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+const bands = new Bands();
+
+bands.addBand(new Band('Imagine Dragons'));
+bands.addBand(new Band('Coldplay'));
+bands.addBand(new Band('One Republic'));
+bands.addBand(new Band('TwentyOne Pilots'));
+
+//console.log(bands);
+
+
 
 io.on('connection', client => {
     console.log('Client connected');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('Client disconnected');
     });
+
+    client.on('vote-band', (payload) => {
+        console.log(payload);
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        console.log(payload);
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        console.log(payload);
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+
+
 });
